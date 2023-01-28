2509a0b7d3bc10acfc0891c296e41c2ebcf2913e
Gwen McArdle <mcardleg@tcd.ie>
2023-01-28 14:42:32 +0000

    REST template in Java 8

--- RiskyLinkBackend/src/test/java/ie/tcd/mcardleg/RiskyLinkBackend/RiskyLinkBackendApplicationTests.java
+++ RiskyLinkBackend/src/test/java/ie/tcd/mcardleg/RiskyLinkBackend/RiskyLinkBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package ie.tcd.mcardleg.RiskyLink_Backend;
+package ie.tcd.mcardleg.RiskyLinkBackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
