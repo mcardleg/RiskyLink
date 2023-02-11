1e9dea4896d52dfbf4a8779158374b508857408e
Gwen McArdle <mcardleg@tcd.ie>
2023-02-11 18:05:55 +0000

    print files endpoint

--- RiskyLinkBackend/src/main/java/ie/tcd/mcardleg/RiskyLinkBackend/RESTController.java
+++ RiskyLinkBackend/src/main/java/ie/tcd/mcardleg/RiskyLinkBackend/RESTController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -20,6 +23,7 @@ import java.util.concurrent.Executors;
 @RestController
 public class RESTController {
     GraphDBHandler graphDBHandler = new GraphDBHandler();
+    String filePath;
 
     @PostMapping("/uploadDataset")
     public ResponseEntity<String> uploadFile(@RequestPart("file") MultipartFile file) {
@@ -31,6 +35,8 @@ public class RESTController {
         try {
             byte[] bytes = file.getBytes();
             Path path = Paths.get(file.getOriginalFilename());
+            filePath = path.toString();
+
             Files.write(path, bytes);
             graphDBHandler.addDataset(path.toString());
         } catch (IOException e) {
@@ -55,6 +61,19 @@ public class RESTController {
             System.out.println(e.getMessage());
         }
         return ResponseEntity.ok("Ontology uploaded.");
+    }
+
+    @GetMapping("/printFiles")
+    public void printFiles() throws IOException {
+        BufferedReader in = new BufferedReader(new FileReader(filePath));
+        String line = in.readLine();
+        while(line != null)
+        {
+            System.out.println(line);
+            line = in.readLine();
+        }
+        in.close();
+
     }
 
     @GetMapping("/generateAlignments")
