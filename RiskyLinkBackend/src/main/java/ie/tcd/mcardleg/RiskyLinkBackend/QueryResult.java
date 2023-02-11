9fe88ea65ae9abda74a4f6b6bd489b11d1b29302
Gwen McArdle <mcardleg@tcd.ie>
2023-02-11 21:38:25 +0000

    fix pojo

--- RiskyLinkBackend/src/main/java/ie/tcd/mcardleg/RiskyLinkBackend/QueryResult.java
+++ RiskyLinkBackend/src/main/java/ie/tcd/mcardleg/RiskyLinkBackend/QueryResult.java
@@ -12,6 +12,30 @@ public class QueryResult {
         this.equivalentClass = equivalentClass;
     }
 
+    public String getDemographic() {
+        return demographic;
+    }
+
+    public void setDemographic(String demographic) {
+        this.demographic = demographic;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getEquivalentClass() {
+        return equivalentClass;
+    }
+
+    public void setEquivalentClass(String equivalentClass) {
+        this.equivalentClass = equivalentClass;
+    }
+
     @Override
     public String toString() {
         return "QueryResult{" +
